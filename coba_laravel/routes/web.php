dad72c74365d1ce56de2a6e4fa424586b9a483ce
taufik545 <63764188+taufik545@users.noreply.github.com>
2021-10-25 11:51:23 +0700

    menyelesaikan struktur folder & view

--- coba_laravel/routes/web.php
+++ coba_laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('About', [
+        "name" => "Taufik Hidayat",
+        "email" => "Taufik193019@gmail.com",
+        "image" => "Taufik.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
